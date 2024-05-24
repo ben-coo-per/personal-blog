f950d990ee4405da58824865749dc4d2c6cf18d3
Ben Cooper <cooperben83@gmail.com>
2024-05-24 11:36:32 -0400

    added analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,13 @@
 	import { ModeWatcher } from 'mode-watcher';
 	import Header from '$lib/components/ui/header.svelte';
 	import '../app.css';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	if (!dev) {
+		// just for funsies, don't worry - I can't see really any details about the people who visit the site, I'll assume most are bots. https://vercel.com/products/observability
+		inject({ mode: 'production' });
+	}
 </script>
 
 <ModeWatcher />
